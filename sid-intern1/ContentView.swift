f43f0a065ea1dfcbc6da693859527521ef4a1030
sid <k.siddhartha.varma@gmail.com>
2019-09-29 18:49:24 +0530

    Initial Commit

---
+++ sid-intern1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sid-intern1
+//
+//  Created by externalOS on 29/09/19.
+//  Copyright © 2019 siddhartha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
